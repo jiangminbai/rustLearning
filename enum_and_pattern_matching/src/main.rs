87a1337187ec5347d1203552f6464e90ada3ac04
jan-wong <543050768@qq.com>
2024-03-03 03:19:06 +0800

    if let

--- enum_and_pattern_matching/src/main.rs
+++ enum_and_pattern_matching/src/main.rs
@@ -21,17 +21,25 @@
  *   a.通配模式other和占位符模式_ 
  *     i.other：涵盖所有其他可能值
  *     ii._:可以匹配任意值而不绑定值，可以使用空元组()作为_分支的代码，表示不做任何操作
- * 
+ * 3.if let else 是match的语法糖
  */
 fn main() {
-    let coin = Coin::Quarter;
+    // let coin = Coin::Quarter;
 
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    };
+    // match coin {
+    //     Coin::Penny => 1,
+    //     Coin::Nickel => 5,
+    //     Coin::Dime => 10,
+    //     Coin::Quarter => 25,
+    // };
+    let config_max: Option<i32> = Some(4);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // };
+    if let Some(x) = config_max {
+        println!("The maximum is configured to be {}", x);
+    }
 }
 
 // enum IpAddrKind {
@@ -47,9 +55,9 @@ fn main() {
 //     Variant1(i32),
 //     Variant2(String),
 // }
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
